ee2e9794c8349179a970534e1a6b3f009761b760
Marius Robert <rwandarushyarobert@gmail.com>
2020-08-15 23:41:51 +0200

    added CRUD operations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
-import post from './model/post'
+import postRoutes from './routes/posts.js'
+
 const app=express();
 
 const PORT=process.env.PORT || 3001;
 
-app.get('/', (req, res)=>{
-    res.json({post});
-});
+app.use(bodyParser.json());
 
 app.listen(PORT, ()=>{
-        console.log(`App is running on port ${PORT}`);
+    console.log(`App is running on port ${PORT}`);
 });
+
+app.get('/', (req, res)=>{
+    res.send('welcome to express');
+});
+
+app.use('/posts', postRoutes);
